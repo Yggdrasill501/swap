28958b5b631603cf29bece48498842ee34f29193
Filip Žitný <filipzitny@gmail.com>
2023-10-04 08:21:35 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
