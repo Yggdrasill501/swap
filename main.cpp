8674955e9e0e46290721dc7808494dd2382c155d
Filip Žitný <filipzitny@gmail.com>
2023-10-04 08:25:38 +0200

    swap 2

--- main.cpp
+++ main.cpp
@@ -6,13 +6,20 @@ void swap(int &a, int &b) {
     b = temp;
 }
 
-void swap(int *a, int *b) {
+void swap2(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 1;
+    int b = 2;
+    swap(a, b);
+    std::cout << a << " " << b << std::endl;
+
+    swap2(&a, &b);
+    std::cout << a << " " << b << std::endl;
+
     return 0;
 }
